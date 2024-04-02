bef1ebffff994ec25649ee539d1ced561879f554
Islam Hany <ihhg.developer@gmail.com>
2024-04-02 16:47:58 +0200

    4. MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+4-mru_cache.py
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache
+    """
+    def __init__(self):
+        """
+        Init.
+        """
+        super().__init__()
+        self.cache_stack = []
+
+    def put(self, key, value):
+        """
+        Puts item to cache.
+        """
+        if not key or not value:
+            return
+
+        cache_is_full = len(self.cache_data) == self.MAX_ITEMS
+        key_is_not_in_cache = key not in self.cache_data.keys()
+
+        if cache_is_full and key_is_not_in_cache:
+            discard = self.cache_stack.pop()
+            del self.cache_data[discard]
+
+            print(f"DISCARD: {discard}")
+
+        self.cache_data[key] = value
+
+        try:
+            self.cache_stack.remove(key)
+        except (ValueError):
+            pass
+
+        self.cache_stack.append(key)
+
+    def get(self, key):
+        """
+        Gets item from cache.
+        """
+        if key and key in self.cache_data.keys():
+            try:
+                self.cache_stack.remove(key)
+            except (ValueError):
+                pass
+
+            self.cache_stack.append(key)
+            return self.cache_data[key]
